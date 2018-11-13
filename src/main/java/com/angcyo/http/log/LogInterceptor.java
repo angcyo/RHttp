4c57868ee1e7c35e7696df810d1a2be91d452992
angcyo <angcyo@126.com>
2018-11-13 15:08:26 +0800

    add log

--- src/main/java/com/angcyo/http/log/LogInterceptor.java
+++ src/main/java/com/angcyo/http/log/LogInterceptor.java
@@ -16,7 +16,16 @@ public class LogInterceptor implements HttpLoggingInterceptorM.Logger {
         INTERCEPTOR_TAG_STR = tag;
     }
 
-    @Override public void log(String message, @LogUtil.LogType int type) {
+    @Override
+    public void log(String message, @LogUtil.LogType int type) {
+        int maxLength = 1024;
+        if (message != null) {
+            int length = message.length();
+            if (length > maxLength) {
+                message = message.substring(0, maxLength);
+                message += " 消息太长,剩余" + (length - maxLength);
+            }
+        }
         LogUtil.printLog(false, type, INTERCEPTOR_TAG_STR, message);
     }
 }
